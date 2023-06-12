945e7efd4b550c5e24bdbef51b49bb7f232e3d8e
Mauricio Godoy <95603787+Mauri-Godoy@users.noreply.github.com>
2023-06-11 23:20:17 -0300

    go links

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,6 +1,6 @@
 export class Constant {
   public static EMAIL: string = 'mauu.godoy01@gmail.com';
-  public static WHATSAPP_NUMBER: string = '2664579830'
+  public static WHATSAPP_NUMBER: string = '5492664579830'
   public static GITHUB_USER: string = 'Mauri-Godoy'
   public static LINKEDIN_USER: string = 'mauri-godoy1'
 }
